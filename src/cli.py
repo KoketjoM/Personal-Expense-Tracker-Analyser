783b1da3c287f7d9e4c1e7e80a131229528611a2
Koketjo Mamashela <kokimamashela@gmail.com>
2026-01-28 10:35:28 +0200

    imported argparse

---
+++ src/cli.py
@@ -0,0 +1,10 @@
+# First created basic input and print statements
+# expense = input("Expense: ")
+# print(expense)
+
+# Decided to use argparser for command line arguments
+# Docs: https://docs.python.org/3/library/argparse.html
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.parse_args()
